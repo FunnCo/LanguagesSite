83eb175164f9434c560d60a55289554adb1091b5
FunnCo <mareev.maksimka@gmail.com>
2022-04-20 19:50:33 +0300

    Added test cases

--- LanguagesSite/test/test_mail.py
+++ LanguagesSite/test/test_mail.py
@@ -1,8 +1,8 @@
 import unittest
 import questionHandler
 
-list_mail_incorrect = ["", "1", "m1@", "@mail", "@", "A@A", "funnco.mar@mail.com"]
-list_mail_correct = ["m.mail@mail.com", "funnco.mar@mail.com", "boab@gmail.ru"]
+list_mail_incorrect = ["", "1", "m1@", "@mail", "@", "A@A", "funnco.@gmail.com", "funnco@hmail.com.", "!^*&^*&%@gmail.com", "\"; SELECT * FROM USERS", "1test@mail.c"]
+list_mail_correct = ["m.mail@mail.com", "funnco.mar@mail.com", "boab@gmail.ru", "1test@mail.com", "test1@mail.com", "zzzzzzzzzzzzzzzz@mail.com", "123123@mail.com"]
 
 class TestStringMethods(unittest.TestCase):
 
