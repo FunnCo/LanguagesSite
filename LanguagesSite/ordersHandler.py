fac6f9f6a1cc3aba87600eaee10116c121815db8
alexdev <alexsenin@internet.ru>
2022-06-02 00:27:08 +0300

    Some fixes...

--- LanguagesSite/ordersHandler.py
+++ LanguagesSite/ordersHandler.py
@@ -1,4 +1,5 @@
 from bottle import post, get, request
+from datetime import datetime
 import json
 import re
 
@@ -13,7 +14,7 @@ def addNewOrder():
 
     if(isPhoneValid(parameters.get('phone'))):
         with open("orders.txt", "a") as myfile:
-            myfile.write(parameters.get("username") + "\n" + parameters.get("order_name") + "\n" + parameters.get("phone") + "\n" + parameters.get("content") + "\n----------\n")
+            myfile.write(parameters.get("username") + "\n" + parameters.get("order_name") + "\n" + parameters.get("phone") + "\n" + parameters.get("content") + "\n" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n----------\n")
     else:
         return {"result": False}
     
@@ -31,6 +32,8 @@ def getAllOrders():
         line = orders_file.readline()
         phone = line
         line = orders_file.readline()
+        time = line
+        line = orders_file.readline()
 
         content = ""
         while line != "----------\n":
@@ -41,6 +44,7 @@ def getAllOrders():
         result_order += content
         result_order += "<br><br><p>Имя пользователя: " + username + "</p>"
         result_order += "<p>Телефон: " + phone + "</p>"
+        result_order += "<p>" + time + "</p>"
         result_order = result_order.replace("\n",'')
         result_order = result_order.replace("\t",'    ')
 
--- LanguagesSite/views/orders.tpl
+++ LanguagesSite/views/orders.tpl
@@ -5,10 +5,10 @@
 	<p class="description">Данная страница предназначена для размещения заказов на публикацию тех или иных статей, связанных с языками программирования и их развитием.</p>
 	<div class="order_container">
 	</div>
-	<div class="center">
+	<div class="center medium-margin-top">
 	<button id="publish">Разместить заказ</button>
 	</div>
-	<div class="center">
+	<div class="center big-margin-top">
 	<button hidden="true" id="publish_cancel">Отменить</button>
 	<button hidden="true" id="publish_confirm" type="submit" form="order_form">Разместить</button>
 	</div>
@@ -18,8 +18,8 @@
 		<p><input class="textinput corners" type="text" id="order_name" name="order_name" size="50" placeholder="Тема заказа"></p>
 		<p><input class="textinput corners" type="text" id="order_phone" name="order_name" size="50" placeholder="Ваш номер телефона"></p>
 		<p><input class="textinput corners" type="text" id="order_username" name="order_name" size="50" placeholder="Ваше имя"></p>
-			</div>
-					<p><textarea class="textinput area-background" id="order_content" name="aritcle_content" placeholder="Описание заказа"></textarea></p>
+		</div>
+		<p><textarea class="textinput area-background" id="order_content" name="aritcle_content" placeholder="Описание заказа"></textarea></p>
 	</div>
 </div>
 
